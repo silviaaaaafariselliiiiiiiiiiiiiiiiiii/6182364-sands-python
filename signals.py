8b5867814db1a1d4234dbf68d41932e0e520fe0a
silviaaaaafariselliiiiiiiiiiiiiiiiiii <silviapaolofariselli.brb@gmail.com>
2025-10-03 01:22:28 +0200

    added sawtooth wave in signals.py

--- signals.py
+++ signals.py
@@ -3,9 +3,18 @@ import numpy as np
 def generate_sine_wave(frequency, duration, sample_rate):
     
     num_samples = int(duration * sample_rate)
-    
     t = np.linspace(0, duration, num_samples, endpoint=False)
     
     sine_wave = np.sin(2 * np.pi * frequency * t)
     
     return sine_wave
+
+def generate_sawtooth_wave(frequency, duration, sample_rate):
+    
+    num_samples = int(duration * sample_rate)
+    t = np.linspace(0, duration, num_samples, endpoint=False)
+    
+    phase = (t * frequency) % 1.0  # Phase from 0 to 1
+    sawtooth_wave = 2 * phase - 1  # Scale from -1 to 1
+    
+    return sawtooth_wave
