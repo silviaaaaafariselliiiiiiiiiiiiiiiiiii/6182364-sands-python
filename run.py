536234c0f7dc9213eb1f8cebdbc273da382c86c6
silviaaaaafariselliiiiiiiiiiiiiiiiiii <silviapaolofariselli.brb@gmail.com>
2025-10-03 03:01:44 +0200

    added frequency and step time

--- run.py
+++ run.py
@@ -4,7 +4,12 @@ from signals import sine_wave, unit_step
 
 # properties of the signal
 duration = 2.0      
-sample_rate = 100   
+sample_rate = 100  
+frequency = 5 
+step_time = 1.0  
+
+sine_signal = sine_wave(frequency, duration, sample_rate)
+step_signal = unit_step(step_time, duration, sample_rate)
 
 # Plot sine wave
 t = np.linspace(0, duration, len(sine_signal))
@@ -23,7 +28,4 @@ plt.title(f'Unit Step: step at {step_time} seconds')
 plt.xlabel('Time (seconds)')
 plt.ylabel('Amplitude')
 plt.grid(True)
-plt.show()
-
-plt.tight_layout()
 plt.show()
