fea882221a77c5f6417ab86cceed053933dac31a
silviaaaaafariselliiiiiiiiiiiiiiiiiii <silviapaolofariselli.brb@gmail.com>
2025-10-03 02:17:05 +0200

    added notes and replaced function for convolution of signals

--- signaloperations.py
+++ signaloperations.py
@@ -1,17 +1,22 @@
 import numpy as np
 
-def amplify(signal, factor):
+#scale the signal by a certain factor
+def amplify(signal, factor): 
     return signal * factor
-
-def time_shift(signal, shift_seconds, sample_rate):
+    
+#shift the signal by a certain time
+def time_shift(signal, shift_seconds, sample_rate): 
     shift_amount = int(shift_seconds * sample_rate)
     return np.roll(signal, shift_amount)
 
+#addition among signals
 def add_signals(signal1, signal2):
     return signal1 + signal2
 
+#multiplication among signals
 def multiply_signals(signal1, signal2):
     return signal1 * signal2
 
-def reverse_signal(signal):
-    return signal[::-1]
+#convolve two signals
+def convolve_signals(signal1, signal2):
+    return np.convolve(signal1, signal2, mode='same')
