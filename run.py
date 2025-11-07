f24024873afe58fd0af3965c115c1587b26539e9
silviaaaaafariselliiiiiiiiiiiiiiiiiii <silviapaolofariselli.brb@gmail.com>
2025-11-07 16:17:51 +0100

    changed the names of the attributes to match all files

--- run.py
+++ run.py
@@ -5,35 +5,35 @@ from signaloperations import *
 
 # properties of the signal
 duration = 2.0      
-sample_rate = 100  
-frequency = 5 
+sample_rate = 1000  
+frequency = 5.0 
 step_time = 1.0  
 
-sine_signal = sine_wave(frequency, duration, sample_rate)
+t, signal = sine_wave(frequency, duration, sample_rate)
 
 # Array of time values 
-t = np.linspace(0, duration, len(sine_signal))
+t = np.linspace(0, duration, len(signal))
 
 # Plot 1: Original Sine Wave
 plt.subplot(2, 2, 1)
-plt.plot(t, sine_signal)
-plt.title('Original Sine Wave')
+plt.plot(t, signal)
+plt.title('Original Wave')
 plt.grid(True)
 
 # Apply operations to sine signal only
-amplified_sine = amplify(sine_signal, factor=2.0)
-shifted_sine = time_shift(sine_signal, shift_seconds=0.5, sample_rate=sample_rate)
+amplified_wave = amplify(signal, factor=2)
+shifted_wave = time_shift(signal, shift_seconds=0.5, sample_rate=sample_rate)
 
 # Plot 2: Amplified Sine Wave
 plt.subplot(2, 2, 2)
-plt.plot(t, amplified_sine)
-plt.title('Amplified Sine Wave')
+plt.plot(t, amplified_wave)
+plt.title('Amplified Wave')
 plt.grid(True)
 
 # Plot 3: Time-Shifted Sine Wave
 plt.subplot(2, 2, 3)
-plt.plot(t, shifted_sine)
-plt.title('Time-Shifted Sine Wave')
+plt.plot(t, shifted_wave)
+plt.title('Time-Shifted Wave')
 plt.grid(True)
 
 plt.tight_layout()
